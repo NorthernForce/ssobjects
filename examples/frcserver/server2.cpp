18b96b4ca5ac95c1ce5cfcbaf2ecf2c42259f30d
Alec Robinson <alecnotalex@ymail.com>
2013-03-08 11:16:31 -0500

    Post Granite State commit

--- examples/frcserver/server2.cpp
+++ examples/frcserver/server2.cpp
@@ -11,9 +11,16 @@
 #include <telnetserversocket.h>
 #include <linkedlist.h>
 #include "timed.h"
+#include <math.h>
 
-#include "/home/pi/OpenCV-2.4.3/modules/highgui/include/opencv2/highgui/highgui.hpp"
+//#include "/home/pi/OpenCV-2.4.3/modules/highgui/include/opencv2/highgui/highgui.hpp"
+//#include "/home/pi/OpenCV-2.4.3/modules/imgproc/include/opencv2/imgproc/imgproc.hpp"
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <stdio.h>
 
@@ -58,23 +65,24 @@ class User
 class ArthurServer : public TelnetServer
 {
 protected:
+  char* infileName;
   unsigned32 m_updates;
   LinkedList<User> m_userList;
   Timed m_timer;  //just used for debugging so we can see the server is active
   StopWatch m_stopwatch;
 
-  bool editcurrent = false; //is true just after the user selects a pixel. when true, the program will get the rgb values of the selected pixel
+  bool editcurrent; //is true just after the user selects a pixel. when true, the program will get the rgb values of the selected pixel
   
-  int thresh = 30; 
+  
   int catnumx, catnumy; //x and y of latest pixel selected by the user
-  int currentpix = 0; //number of selected pixels
+  int currentpix; //number of selected pixels
   int bee[99], jee[99], are[99]; //rgb values of selected pixels (at the time of selection)
 
-  bool findcenter = true;
+  bool findcenter;
   int runningx, runningy;
   float avrgx, avrgy; //for averaging; running total of x values, y values, and the calculated averages
 
-  void my_mouse_callback( int event, int x, int y, int flags, void* param );
+  //void my_mouse_callback( int event, int x, int y, int flags, void* param );
 
   VideoCapture capturen; 
   Mat frame; //mat of the original image
@@ -82,119 +90,19 @@ protected:
   
 
     public:
+                int thresh; 
 		enum {FREQUENCY = 33};    //in milliseconds - 33 is about 30 times per second 20 is about the maximum
-		ArthurServer(SockAddr& saBind) : TelnetServer(saBind,FREQUENCY),m_timer(1000),m_stopwatch() 
+		ArthurServer(SockAddr& saBind, char* infileName) : TelnetServer(saBind,FREQUENCY),m_timer(1000),m_stopwatch(), infileName(infileName)
         {
+	  
+	  findcenter = true;
             m_stopwatch.start();
+	    targetinginit();
         }
 
     protected:
 
-   void targetinginit ()
-  {
-    cout << "Left click to collect samples, right click to clear them.\n";
-    cout << "'c' to toggle center finding.\n";
-    cout << "Press q to quit, but only if you really mean it.\n";
-
-    std::string arg = "0";
-    VideoCapture capture(arg); 
-    if (!capture.isOpened())
-        capture.open(atoi(arg.c_str()));
-    capture.set(CV_CAP_PROP_FRAME_WIDTH, 240); //this line and the following line is necessary only for the raspberry pi. if you were to delete these on the pi, you would get timeout errors
-	capture.set(CV_CAP_PROP_FRAME_HEIGHT, 320);
-    if (!capture.isOpened()) {
-      cerr << "Failed to open a video device or video file!\n" << endl;
-    }
-    capturen = capture;
-
-    namedWindow("Original Image", CV_WINDOW_KEEPRATIO);
-    namedWindow("Threshold Image", CV_WINDOW_KEEPRATIO);
-    
-       
-    cvCreateTrackbar( "Threshold", "Threshold Image", &thresh, 100, NULL );
-  }
-
-
-
-int targetingprocess (float &azimuth, float &elevation, int &npix)
-  {
-
-    npix = 10;
-            
-      capturen >> newframe;
-
-	    	    
-	  if (editcurrent) //reads the rgb of the newly selected pixel
-	      {
-		int z = currentpix-1;
-		bee[z] = newframe.at<cv::Vec3b>(catnumx,catnumy)[0];
-		jee[z] = newframe.at<cv::Vec3b>(catnumx,catnumy)[1];
-		are[z] = newframe.at<cv::Vec3b>(catnumx,catnumy)[2];
-		editcurrent = false;
-	      }
-	      
-
-	    int found = 0; //number of matches found (used for averaging)
-	    runningx = 0;
-	    runningy = 0;
-	    for(int i = 0; i < frame.rows; i++)
-	    {
-	      for(int j = 0; j < frame.cols; j++)
-		{
-		  int thisbee = newframe.at<cv::Vec3b>(i,j)[0];
-		  int thisjee = newframe.at<cv::Vec3b>(i,j)[1];
-		  int thisare = newframe.at<cv::Vec3b>(i,j)[2];
-		  int matches = 0;
-		  for(int h = 0; h <= currentpix; h++)
-		    {
-
-		      if(catnumx+catnumy != 0) if ((thisbee<bee[h]+thresh && thisbee>bee[h]-thresh) && (thisjee<jee[h]+thresh && thisjee>jee[h]-thresh) && (thisare<are[h]+thresh && thisare>are[h]-thresh)) //if it's a match
-			{
-			  newframe.at<cv::Vec3b>(i,j)[0] = 255; //set to white
-			  newframe.at<cv::Vec3b>(i,j)[1] = 255;
-			  newframe.at<cv::Vec3b>(i,j)[2] = 255;
-			  if (findcenter && matches==0) {runningx += i; runningy += j; found++;}
-			  matches++;
-			}
-		     
-		    }
-		  if (matches == 0)
-			{
-			  newframe.at<cv::Vec3b>(i,j)[0] = 0;
-			  newframe.at<cv::Vec3b>(i,j)[1] = 0;
-			  newframe.at<cv::Vec3b>(i,j)[2] = 0;
-			}
-		}
-		  
-	    
-	    }
-	    if (findcenter)
-	      {  
-		if (found>0){avrgx = runningx/found; //average the x
-		  avrgy = runningy/found;} //average the y
-		runningx = 0;
-		runningy = 0;
-		Point thecenter = Point(avrgy, avrgx);
-		ellipse(newframe, thecenter, Size(20,20), 0, 0, 360, Scalar(255,0,238), 2,8); //draw circle
-	      }
-	    imshow("Threshold Image", newframe);
-	    capturen >> frame;
-	    if (findcenter) { Point thecenter = Point(avrgy, avrgx); ellipse(frame, thecenter, Size(20,20), 0, 0, 360, Scalar(255,0,238), 2,8);}
-
-            imshow("Original Image", frame);
-	    IplImage ipl = frame;
-	    cvSetMouseCallback( "Original Image", my_mouse_callback, (void*) &ipl); //get mouse input
-	   
-            char key = (char)waitKey(5); //get keyboard input
-	    if (key == 'q') break;
-	    if (key == 'c') {findcenter = !findcenter; runningx = 0; runningy = 0; avrgx = 0; avrgy = 0;}
-	    azimuth = avrgx;
-	    elevation = avrgy;
-        return 0;
-  }
-	
-
-  void my_mouse_callback( int event, int x, int y, int flags, void* param ){
+  MouseCallback my_mouse_callback( int event, int x, int y, int flags, void* param ){
 	IplImage* image = (IplImage*) param;
 
 	switch( event ){
@@ -216,6 +124,188 @@ int targetingprocess (float &azimuth, float &elevation, int &npix)
 		  break;
 	}
   }
+
+   void targetinginit ()
+  {
+
+
+    std::string arg = "0";
+    VideoCapture capture(arg); 
+    if (!capture.isOpened())
+        capture.open(atoi(arg.c_str()));
+    capture.set(CV_CAP_PROP_FRAME_WIDTH, 240); //this line and the following line is necessary only for the raspberry pi. if you were to delete these on the pi, you would get timeout errors
+	capture.set(CV_CAP_PROP_FRAME_HEIGHT, 320);
+    if (!capture.isOpened()) {
+      cerr << "Failed to open a video device or video file!\n" << endl;
+    }
+    capturen = capture;
+
+    //namedWindow("Original Image", CV_WINDOW_KEEPRATIO);
+    //namedWindow("Threshold Image", CV_WINDOW_KEEPRATIO);
+    
+       
+    //cvCreateTrackbar( "Threshold", "Threshold Image", &thresh, 100, NULL );
+
+    cout << "Loading BGR values...\n";
+
+    ifstream infile;
+    infile.open(infileName);
+    string line;
+    int somenumber = -1;
+    
+    while (infile.good())
+      {
+	//cout << "Loading result...\n";
+	int result;
+	getline(infile,line);
+	stringstream convert(line);
+	if ( !(convert >> result) )
+	  result = 0;
+	
+	if (somenumber == -1)
+	  {
+	    thresh = result;
+	    somenumber++;
+	    //cout << "Loaded threshhold!\n";
+	  }
+
+	else if (somenumber == 0)
+	  {
+	    //cout << "B" << currentpix << " is " << result << "\n";
+	    bee[currentpix] = result;
+	    somenumber++;
+	  }
+	else if (somenumber == 1)
+	  {
+	    //cout << "G" << currentpix << " is " << result << "\n";
+	    jee[currentpix] = result;
+	    somenumber++;
+	  }
+	else if (somenumber == 2)
+	  {
+	    //cout << "R" << currentpix << " is " << result << "\n";
+	    are[currentpix] = result;
+	    somenumber = 0;
+	    currentpix++;
+	  }
+      }
+    //cout << "Done loading BGR file!\n";
+	
+  }
+
+
+
+int targetingprocess (float &azimuth, float &elevation, int &npix)
+  {
+
+    npix = 0;
+            
+      capturen >> newframe;
+
+      /*
+	  if (editcurrent) //reads the rgb of the newly selected pixel
+	      {
+		int z = currentpix-1;
+		bee[z] = newframe.at<cv::Vec3b>(catnumx,catnumy)[0];
+		jee[z] = newframe.at<cv::Vec3b>(catnumx,catnumy)[1];
+		are[z] = newframe.at<cv::Vec3b>(catnumx,catnumy)[2];
+		editcurrent = false;
+	      }
+	      
+      */
+	    int found = 0; //number of matches found (used for averaging)
+	    runningx = 0;
+	    runningy = 0;
+	    for(int i = 0; i < frame.rows; i++)
+	    {
+	      for(int j = 0; j < frame.cols; j++)
+		{
+		  int thisbee = newframe.at<cv::Vec3b>(i,j)[0];
+		  int thisjee = newframe.at<cv::Vec3b>(i,j)[1];
+		  int thisare = newframe.at<cv::Vec3b>(i,j)[2];
+		  int matches = 0;
+		  for(int h = 0; h <= currentpix; h++)
+		    {
+		     
+		       if ((thisbee<bee[h]+thresh && thisbee>bee[h]-thresh) && (thisjee<jee[h]+thresh && thisjee>jee[h]-thresh) && (thisare<are[h]+thresh && thisare>are[h]-thresh)) //if it's a match
+			{
+			  newframe.at<cv::Vec3b>(i,j)[0] = 255; //set to white
+			  newframe.at<cv::Vec3b>(i,j)[1] = 255;
+			  newframe.at<cv::Vec3b>(i,j)[2] = 255;
+			  if (findcenter && matches==0) {runningx += i; runningy += j; found++;}
+			  matches++;
+			}
+		     
+		    }
+		  if (matches == 0)
+			{
+			  newframe.at<cv::Vec3b>(i,j)[0] = 0;
+			  newframe.at<cv::Vec3b>(i,j)[1] = 0;
+			  newframe.at<cv::Vec3b>(i,j)[2] = 0;
+			}
+		}
+		  
+	    
+	    }
+
+	    Mat newerframe;
+	    erode(newframe, newerframe, Mat());
+	    
+	    dilate(newerframe, newerframe, Mat());
+
+	    found = 0;
+	    runningx = 0;
+	    runningy = 0;
+
+	    for(int i = 0; i < frame.rows; i++)
+	    {
+	      for(int j = 0; j < frame.cols; j++)
+		{
+		  if (newerframe.at<cv::Vec3b>(i,j)[0] == 255 &&
+		      newerframe.at<cv::Vec3b>(i,j)[1] == 255 &&
+		      newerframe.at<cv::Vec3b>(i,j)[2] == 255)
+		    {
+		      runningx+=i;
+		      runningy+=j;
+		      found++;
+		    }
+		}
+	    }
+
+	    
+
+	    if (findcenter)
+	      {  
+		if (found>0){avrgx = runningx/found; //average the x
+		  avrgy = runningy/found;} //average the y
+		runningx = 0;
+		runningy = 0;
+		Point thecenter = Point(avrgy, avrgx);
+		ellipse(newerframe, thecenter, Size(20,20), 0, 0, 360, Scalar(255,0,238), 2,8); //draw circle
+	      }
+	    //imshow("Threshold Image", newframe);
+	    capturen >> frame;
+	    if (findcenter) { Point thecenter = Point(avrgy, avrgx); ellipse(frame, thecenter, Size(20,20), 0, 0, 360, Scalar(255,0,238), 2,8);}
+
+            //imshow("Original Image", frame);
+	    IplImage ipl = frame;
+	    //cvSetMouseCallback( "Original Image", (cv::MouseCallback) &ArthurServer::my_mouse_callback, (void*) &ipl); //get mouse input
+	   
+            //char key = (char)waitKey(5); //get keyboard input
+	   
+	    //if (key == 'c') {findcenter = !findcenter; runningx = 0; runningy = 0; avrgx = 0; avrgy = 0;}
+
+	    //int offsetx = (avrgx-frame.cols/2)*-1;
+	    //int offsety = (avrgy-frame.rows/2)*-1;
+
+	    azimuth = atan((1.0/203.0) * (avrgx - 91.0)) * (180.0 / M_PI);
+	    elevation = atan((1.0/210.0) * (avrgy - 77.0)) * (180.0 / M_PI);
+	    npix = found;
+        return 0;
+  }
+	
+
+  
 
 		void processSingleMsg(PacketMessage* pmsg)
         {
@@ -258,7 +348,7 @@ int targetingprocess (float &azimuth, float &elevation, int &npix)
             {
                 char cmd[5];
                 strncpy(cmd,pstring,4);
-                cmd[4] = NULL;  //null terminate
+                cmd[4] = '\0';  //null terminate
                 printf("Got from client: cmd [%s] line [%s]\n",cmd,pstring);
                 processCommand(psocket,cmd,pstring);
             }
@@ -276,7 +366,10 @@ int targetingprocess (float &azimuth, float &elevation, int &npix)
         {
 //            if(m_timer.update())
 //                printf("%06lu tick\n",m_stopwatch.milliseconds());
-            sendCameraUpdates((unsigned long)now);
+	  float az = 40, ele = 50; 
+	  int npix = 80;
+	  targetingprocess(az, ele, npix);
+	  sendCameraUpdates((unsigned long)now, az, ele, npix);
         }
 
         void processCommand(TelnetServerSocket* psocket,const char* pcmd,char* pstring)
@@ -311,12 +404,12 @@ int targetingprocess (float &azimuth, float &elevation, int &npix)
         }
 
         //Send the camera information to whoever wants it
-        void sendCameraUpdates(unsigned long now)
+  void sendCameraUpdates(unsigned long now, float azimuth, float elevation, int Npix)
         {
             char buff[1024];
             //const char* msg = "CAMC angle1,angle2,distance,x,y,orientation";
-            const char* msg = "CAMC 12.3, 14.2, 20.1, 4, 5, 30.1";
-	    snprintf(buff,sizeof buff,"%s,%lu\n",msg,now);  //check your docs, to make sure this automatically is null terminated
+            const char* msg = "CAMC 1,%f,%f,%d\n";
+	    snprintf(buff,sizeof buff,msg,azimuth,elevation,Npix);  //check your docs, to make sure this automatically is null terminated
 
             User* puser = m_userList.getHead();
             while(puser)
@@ -329,22 +422,27 @@ int targetingprocess (float &azimuth, float &elevation, int &npix)
         }
 };
 
-int main(int argc, char const *argv[])
+int main(int argc,char *argv[])
 {
 	try 
 	{
 		unsigned16 wPort = 1180;
 
 		SockAddr saBind((ULONG)INADDR_ANY,wPort);
-		ArthurServer server(saBind);          
+		ArthurServer server(saBind, argv[1]);          
     	if(!SimpleServer::canBind(saBind))  // check if we can bind to this port
       		printf("Can't bind\n");       // should not throw from main after server constructed
         else
         {
-            printf("Server on port %d\n",wPort);
-            printf("You can run 'telnet localhost %u' from another termainal.\n",wPort);
-            server.startServer();               // server will now listen for connections
-            printf("server is finished.\n");
+	  // int threshhold = atoi(argv[1]);
+	  //if (argc < 2) printf("You need to specify a threshhold!\n");
+	  //else {
+	  printf("Server on port %d\n",wPort);
+	  printf("You can run 'telnet localhost %u' from another terminal.\n",wPort);
+	  //     server.thresh = threshhold;
+                 server.startServer();               // server will now listen for connections
+                 printf("server is finished.\n");
+		 //   }
 	    }
 	} 
 	catch (GeneralException& e)
